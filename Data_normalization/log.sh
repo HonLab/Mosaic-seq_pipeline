825f7983177b04d47801e7cc415496861b3c5464
Russell Xie <russellxie@gmail.com>
2017-03-09 13:58:20 -0600

    Update and rename log to log.sh

--- Data_normalization/log.sh
+++ Data_normalization/log.sh
@@ -7,10 +7,10 @@ foreach counter (1 2 3 4 5 6)
 
 	cd sorted_dir_$counter
 	
-	mv ../log$counter .
+	mv ../log$counter.sh .
 	cp ../*pl .
 	cp ../*m .
-	sbatch log$counter
+	sbatch log$counter.sh
 
 	cd ../
 
